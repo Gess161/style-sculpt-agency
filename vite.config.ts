31ca8de818b08ab2494048fe395979c996f5a189
Hennadii Kotulai <hennadii.kotulai@quarks.tech>
2025-10-01 23:34:29 +0300

    Fixes on vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/style-sculpt-agency/", // Added for GitHub Pages deployment
+  base: "/",
   server: {
     host: "::",
     port: 8080,
