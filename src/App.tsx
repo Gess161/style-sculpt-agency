95874497aff4155dd1ea5166bd414169554b0dd8
Hennadii Kotulai <hennadii.kotulai@quarks.tech>
2025-10-01 22:27:05 +0300

    changes for deployment

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ const App = () => (
       <Sonner />
       <BrowserRouter>
         <Routes>
-          <Route path="/style-sculpt-agency" element={<Index />} />
+          <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
